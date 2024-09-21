7d7844a8dc4aceb5d1f715d2884287f7135c9425
Cain Martinez Bernabeu <cainmartinez2@hotmail.com>
2024-09-21 16:26:56 +0200

    Front con Angular, añadidas las pages Home y Shop, creados los componentes de header y footer.

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent, HeaderComponent, SharedModule } from './shared';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
